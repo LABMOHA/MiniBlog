27052bad7638f58f5747e8fb6e3c9e1fbb0eada7
LABMOHA <mlabbou25@gmail.com>
2025-12-19 17:13:10 +0100

    add login and register pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
+
+
+Route::get('/login', [])->name('show.login');
+Route::get('/register', [])->name('show.register');
+
 Route::get('/', [ArticleController::class, 'index']);
 Route::resource('articles', App\Http\Controllers\ArticleController::class);
